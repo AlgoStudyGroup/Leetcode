fae420ba8e4d23cc69f0f6d58c14a9cc4fa50ce6
Qilin ZHANG <nicolas.qilin.zhang@gmail.com>
2020-05-03 13:44:00 +0200

    Split the different java solution in different class

--- May-LeetCoding-Challenge/02-Jewels-And-Stones/Jewels-And-Stones.java
+++ May-LeetCoding-Challenge/02-Jewels-And-Stones/Jewels-And-Stones.java
@@ -1,8 +1,8 @@
 class Solution {
-	
     public int numJewelsInStones(String J, String S) {
         if (J == null || S==null) return 0;
         Set<Character> jmap = new HashSet<>();
+        
         //time O(len of J)
         //space O(len of J)
         for (int i = 0; i<J.length(); i++){
@@ -15,22 +15,12 @@ class Solution {
             if(jmap.contains(S.charAt(i))) count++;
         }
 
-        /*
-        chat stone;
-        for (int i = 0; i < S.length(); i++) {
-            stone = S.charAt(i);
-            if(J.indexOf(stone) != -1){
-                count++;
-            }
-        }*/
-
         return count;
     }
 }
 
 // String solution
 class Solution {
-	
     public int numJewelsInStones(String J, String S) {
         if (J == null || S==null) return 0;
         int count = 0;

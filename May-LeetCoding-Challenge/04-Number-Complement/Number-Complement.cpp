0cdc85306a5d0baa1d69c61a218010ddf29dc355
Mincong HUANG <mincong.h@gmail.com>
2020-05-04 21:11:04 +0200

    Move C++ solution into a single file

--- May-LeetCoding-Challenge/04-Number-Complement/Number-Complement.cpp
+++ May-LeetCoding-Challenge/04-Number-Complement/Number-Complement.cpp
@@ -6,3 +6,20 @@ public:
         return (i-1) ^ num;
     }
 };
+
+class Solution2 {
+private:
+    inline int fill_bits(int v) {
+        v |= v >> 1;
+        v |= v >> 2;
+        v |= v >> 4;
+        v |= v >> 8;
+        v |= v >> 16;
+        return v;
+    }
+public:
+
+    int findComplement(int num) {
+        return fill_bits(num + (num==0) ) & ~num;
+    }
+};

19157aef2736968850f877c35a094ffdfcae79ca
jinshendan <jinshendan@gmail.com>
2020-05-05 22:01:10 +0200

    improve cpp solution

--- May-LeetCoding-Challenge/05-First-Unique-Character-In-A-String/First-Unique-Character-In-A-String.cpp
+++ May-LeetCoding-Challenge/05-First-Unique-Character-In-A-String/First-Unique-Character-In-A-String.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int firstUniqChar(string s) {
         vector<int> num(26, 0);
-        int ans = -1;
         for (auto &t: s) num[t - 'a']++;
         for (int i = 0; i < s.length(); i++)
             if (num[s[i] - 'a'] == 1) return i;

ed564dfd046df4b922a0fe075a469d5ffcbb62b1
kerms <kailun1997@qq.com>
2020-05-05 15:41:12 +0200

    [May 05, 2020] add cpp solution
    iterate only 1 time the string.

--- May-LeetCoding-Challenge/05-First-Unique-Character-In-A-String/First-Unique-Character-In-A-String.cpp
+++ May-LeetCoding-Challenge/05-First-Unique-Character-In-A-String/First-Unique-Character-In-A-String.cpp
@@ -9,3 +9,31 @@ public:
         return -1;
     }
 };
+
+class Solution2 {
+public:
+    int firstUniqChar(string s) {
+        /*  
+         * O(n + 26)
+         */
+        
+        int first = -1;
+        int tab[26];
+        
+        memset(tab, -1, 26*sizeof(int));
+        
+        for (int i = 0 ; i < s.size() ; ++i) {
+            if (tab[s[i] - 'a'] == -1) {
+                tab[s[i] - 'a'] = i;
+            } else if (tab[s[i] - 'a'] != -2){
+                tab[s[i] - 'a'] = -2;
+            }
+        }
+        
+        for (int i = 0 ; i < 26 ; ++i) {
+            first = min((unsigned int)tab[i], (unsigned int)first);
+        }
+        
+        return first + (first == -2);
+    }
+};

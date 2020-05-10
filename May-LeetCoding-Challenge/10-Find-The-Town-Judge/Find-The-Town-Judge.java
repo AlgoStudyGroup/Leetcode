d88e0c98d9bae410aecdca070966a66821c80071
Mincong HUANG <mincong.h@gmail.com>
2020-05-10 21:34:10 +0200

    D10: Find-The-Town-Judge (Java)

--- May-LeetCoding-Challenge/10-Find-The-Town-Judge/Find-The-Town-Judge.java
+++ May-LeetCoding-Challenge/10-Find-The-Town-Judge/Find-The-Town-Judge.java
@@ -45,3 +45,19 @@ class Solution2 {
         
     }
 }
+
+class Solution3 {
+    public int findJudge(int N, int[][] trust) {
+        int[] count = new int[N + 1];
+        for (int[] t : trust) {
+            count[t[0]]--;
+            count[t[1]]++;
+        }
+        for (int i = 1; i <= N; i++) {
+            if (count[i] == N - 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}

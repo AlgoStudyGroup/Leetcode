23dd142c963765c71357f8dadc8222e5f9a44b3d
jinshendan <jinshendan@gmail.com>
2020-05-13 09:55:49 +0200

    add cpp solution

---
+++ May-LeetCoding-Challenge/13-Remove-K-Digits/Remove-K-Digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        string ans;
+        for (auto& c : num){
+            while (k and ans.length() and c < ans[ans.length() - 1]) {
+                ans.pop_back();
+                k--;
+            }
+            if (ans.length() == 0 and c == '0') continue;
+            ans += c;
+        }
+        
+        while (k-- and ans.length()) ans.pop_back();
+        
+        return ans.length()?ans:"0";
+        
+    }
+};

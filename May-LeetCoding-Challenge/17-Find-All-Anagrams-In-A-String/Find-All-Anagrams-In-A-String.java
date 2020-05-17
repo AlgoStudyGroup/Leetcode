87e3eee441da27ee6d56ee900d536799f63b71a9
Qilin ZHANG <nicolas.qilin.zhang@gmail.com>
2020-05-17 18:21:12 +0200

    Add java solution2 for day16: Find All Anagrams in a String

--- May-LeetCoding-Challenge/17-Find-All-Anagrams-In-A-String/Find-All-Anagrams-In-A-String.java
+++ May-LeetCoding-Challenge/17-Find-All-Anagrams-In-A-String/Find-All-Anagrams-In-A-String.java
@@ -38,3 +38,35 @@ class Solution {
         return Arrays.equals(strCount, pCount);
     }
 }
+
+class Solution2 {    
+    boolean isAnagram (String a, String b) {
+        int[] arrayA = new int[26];
+        int[] arrayB = new int[26];
+
+        for (int i = 0; i < a.length(); i++) {
+            arrayA[a.charAt(i) - 'a']++;
+        }
+
+        for (int j = 0; j < b.length(); j++) {
+            arrayB[b.charAt(j) - 'a']++;
+        }
+        
+        for (int k = 0; k < 26; k++) {
+            if(arrayA[k] != arrayB[k])  return false;
+        }
+        return true;
+    }
+    
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res = new ArrayList<>();
+        //List<String> listSubString = new ArrayList<>();
+        String subString = null;
+        for (int i = 0; i < s.length() - p.length() + 1; i++) {
+            subString = s.substring(i, p.length() + i);
+             if(isAnagram(subString, p)) res.add(i);
+        }
+        
+        return res;
+    }
+}

1fddd553c6f3c42342932127ae36df97491edebe
jinshendan <jinshendan@gmail.com>
2020-05-21 16:14:45 +0200

    add cpp solution 2

--- May-LeetCoding-Challenge/21-Count-Square-Submatrices-With-All-Ones/Count-Square-Submatrices-With-All-Ones.cpp
+++ May-LeetCoding-Challenge/21-Count-Square-Submatrices-With-All-Ones/Count-Square-Submatrices-With-All-Ones.cpp
@@ -23,3 +23,26 @@ public:
         
     }
 };
+
+class Solution2 {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        if (n == 0) return 0;
+        int m = matrix[0].size();
+        
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        
+        
+        int ans = 0;
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= m; j++){
+                if (matrix[i-1][j-1] == 0) dp[i][j] = 0;
+                else dp[i][j] = 1 + min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]));
+              
+                ans += dp[i][j];
+            }
+        return ans;
+        
+    }
+};

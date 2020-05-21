171652b7b34d1944f840fa0eb85255165ac0d286
Mincong HUANG <mincong.h@gmail.com>
2020-05-21 09:24:32 +0200

    Add Kth smallest

--- May-LeetCoding-Challenge/20-Kth-Smallest-Element-In-A-BST/Kth-Smallest-Element-In-A-BST.java
+++ May-LeetCoding-Challenge/20-Kth-Smallest-Element-In-A-BST/Kth-Smallest-Element-In-A-BST.java
@@ -30,3 +30,32 @@ class Solution1 {
         return array.get(k-1);
     }
 }
+
+class Solution2 {
+    public int kthSmallest(TreeNode root, int k) {
+        return walk(root, k)[0];
+    }
+
+    /**
+     * Finds the Kth smallest element by walking through
+     * the tree in in-order: left, node, right (LNR).
+     *
+     * @return a pair containing the candidate for the Kth smallest
+     *         value and the remaining number of nodes to visit
+     */
+    private int[] walk(TreeNode root, int k) {
+        if (root == null) {
+            return new int[] { Integer.MAX_VALUE, k };
+        }
+        int[] arr = walk(root.left, k);
+        int candidate = arr[0];
+        int remaining = arr[1];
+        if (remaining == 0) {
+            return new int[] { candidate, 0 };
+        }
+        if (remaining == 1) {
+            return new int[] { root.val, 0 };
+        }
+        return walk(root.right, remaining - 1);
+    }
+}

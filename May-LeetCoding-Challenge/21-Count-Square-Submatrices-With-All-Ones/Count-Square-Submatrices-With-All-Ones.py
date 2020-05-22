50e465ab13692184bbb889c2f33954ddd06bc7f1
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2020-05-22 22:07:51 +0200

    Add python solution

---
+++ May-LeetCoding-Challenge/21-Count-Square-Submatrices-With-All-Ones/Count-Square-Submatrices-With-All-Ones.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        nrows = len(matrix)
+        ncols = len(matrix[0])
+
+        output = 0
+        for r in range(nrows):
+            for c in range(ncols):
+                if matrix[r][c] == 0:
+                    pass
+                elif r == 0 or c == 0:
+                    output += 1
+                else:
+                    min_neighbour = min(matrix[r][c - 1],
+                                        matrix[r - 1][c - 1],
+                                        matrix[r - 1][c])
+                    matrix[r][c] += min_neighbour
+                    output += matrix[r][c]
+
+        return output
+s

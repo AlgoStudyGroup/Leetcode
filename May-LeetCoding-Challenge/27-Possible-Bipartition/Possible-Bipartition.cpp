574f2794894425b37686807a27547f2f1b955f11
jinshendan <jinshendan@gmail.com>
2020-05-27 09:52:18 +0200

    improve cpp

--- May-LeetCoding-Challenge/27-Possible-Bipartition/Possible-Bipartition.cpp
+++ May-LeetCoding-Challenge/27-Possible-Bipartition/Possible-Bipartition.cpp
@@ -4,16 +4,16 @@ public:
     vector<vector<int>> G;
     
     bool bipartite(int u) {
-	for (int i = 0; i < G[u].size(); i++) {
-		int v = G[u][i];
-		if (color[v] == color[u]) return false;
-		if (!color[v]) {
-			color[v] = 3 - color[u];
-			if (!bipartite(v)) return false;
-		}
-	}
-	return true;
-}
+        for (int i = 0; i < G[u].size(); i++) {
+            int v = G[u][i];
+            if (color[v] == color[u]) return false;
+            if (!color[v]) {
+                color[v] = 3 - color[u];
+                if (!bipartite(v)) return false;
+            }
+        }
+        return true;
+    }
     
     bool possibleBipartition(int N, vector<vector<int>>& dislikes) {
         color.resize(N+1);

e2b0a43cc6b379f2dc29baa48a6f531f7bedd579
supaggregator <duo.xu@hotmail.com>
2020-05-30 23:50:29 +0200

    Update K-Closest-Points-To-Origin.scala

--- May-LeetCoding-Challenge/30-K-Closest-Points-To-Origin/K-Closest-Points-To-Origin.scala
+++ May-LeetCoding-Challenge/30-K-Closest-Points-To-Origin/K-Closest-Points-To-Origin.scala
@@ -1,7 +1,7 @@
 object Solution {
     
     def kClosest(points: Array[Array[Int]], K: Int): Array[Array[Int]] =
-    points.toList.sortWith(sortBySqrt).take(K).toArray
+    points.sortWith(sortBySqrt).take(K)
     
     private def sortBySqrt(arr1 :Array[Int], arr2:Array[Int]): Boolean = 
     (arr1(0) * arr1(0) + arr1(1) * arr1(1)) < (arr2(0) * arr2(0) + arr2(1) * arr2(1))

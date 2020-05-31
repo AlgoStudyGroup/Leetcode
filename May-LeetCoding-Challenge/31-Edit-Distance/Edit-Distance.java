7f710b40cb38e6453b321f71d580d55454b714bd
Qilin ZHANG <nicolas.qilin.zhang@gmail.com>
2020-05-31 15:23:05 +0200

    Add comments for Java solution of day31

--- May-LeetCoding-Challenge/31-Edit-Distance/Edit-Distance.java
+++ May-LeetCoding-Challenge/31-Edit-Distance/Edit-Distance.java
@@ -1,8 +1,22 @@
+/**
+ * Dynamic programming, operation of previous string plus one if the last character is not the same
+ * XXXb -> YYYc => operation(XXX to YYY) + 1
+ * XXXm -> XXXm => operation(XXX to YYY)
+ */
 class Solution {
     public int minDistance(String word1, String word2) {
         int m = word1.length(), n = word2.length();
-        
         int dp[][] = new int[m+1][n+1];
+
+        /**
+         * Construct a dp table
+         * __|''|a|b|c|
+         * ''| 0|1|2|3|...
+         * ''|0
+         * d |1
+         * e |2
+         * f |3
+         */
         for (int i = 0; i <= m; i++) {
             dp[i][0] = i;
         }
@@ -16,9 +30,11 @@ class Solution {
                 char c1 = word1.charAt(i);
                 char c2 = word2.charAt(j);
 
+                // If the last character is the same, we do nothing compare to previous string
                 if (c1 == c2) {
                     dp[i + 1][j + 1] = dp[i][j];
                 } else {
+                    // we chose the minimum operation of three operation: insert, delete, replace
                     dp[i + 1][j + 1] = Math.min(dp[i][j], Math.min(dp[i+1][j], dp[i][j+1])) + 1;
                 }
             }

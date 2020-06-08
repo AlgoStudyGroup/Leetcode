c3a9a5f95905c1f00d355a667464178000985ad1
FZ YUAN <42092999+FZYUAN-1@users.noreply.github.com>
2020-06-08 10:22:44 +0200

    Update Power-Of-Two.cpp

--- June-LeetCoding-Challenge/08-Power-Of-Two/Power-Of-Two.cpp
+++ June-LeetCoding-Challenge/08-Power-Of-Two/Power-Of-Two.cpp
@@ -6,3 +6,24 @@ public:
         return (x & (-x)) == x;
     }
 };
+
+class Solution {
+public:
+    int right_most_setbit(int num) { 
+        int pos = 0; 
+        // counting the position of first set bit 
+        for (int i = 0; i < 32; i++) { 
+            if (!(num & (1 << i))) 
+                pos++; 
+            else
+                break; 
+        } 
+        return pos; 
+    }
+    
+    bool isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        else return right_most_setbit(n) == int(log2(n));
+        
+    }
+};

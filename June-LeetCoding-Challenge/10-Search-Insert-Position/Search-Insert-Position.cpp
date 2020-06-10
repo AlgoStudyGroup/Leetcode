3f60121c59edc32a7e1a32879153edaa20f6416e
jinshendan <jinshendan@gmail.com>
2020-06-10 10:20:51 +0200

    add cpp solution2

--- June-LeetCoding-Challenge/10-Search-Insert-Position/Search-Insert-Position.cpp
+++ June-LeetCoding-Challenge/10-Search-Insert-Position/Search-Insert-Position.cpp
@@ -4,3 +4,11 @@ public:
         return distance(nums.begin(), lower_bound(nums.begin(), nums.end(), target));
     }
 };
+
+
+class Solution2 {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+       	return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+    }
+};

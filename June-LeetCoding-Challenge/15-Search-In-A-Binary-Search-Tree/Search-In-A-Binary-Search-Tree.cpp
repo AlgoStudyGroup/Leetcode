5e524d843deefa867de701e0af61beb6a5cfc299
FZ YUAN <42092999+FZYUAN-1@users.noreply.github.com>
2020-06-15 09:33:18 +0200

    Update Search-In-A-Binary-Search-Tree.cpp

--- June-LeetCoding-Challenge/15-Search-In-A-Binary-Search-Tree/Search-In-A-Binary-Search-Tree.cpp
+++ June-LeetCoding-Challenge/15-Search-In-A-Binary-Search-Tree/Search-In-A-Binary-Search-Tree.cpp
@@ -8,3 +8,27 @@ public:
         return NULL;
     }
 };
+
+class Solution2 {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        stack<TreeNode*> st;
+        
+        while(!st.empty() || root){
+            while(root){
+                st.push(root);
+                root = root->left;
+            }
+            
+            TreeNode* t = st.top();
+            if(t->val == val) return t;
+            if(t->val > val) return nullptr;
+            st.pop();
+            
+            if(t->right)
+                root = t->right;
+            
+        }
+        return nullptr;
+    }
+};

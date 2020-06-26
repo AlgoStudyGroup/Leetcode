4804ec81be6669f9d59b2716703fe2c63839bcf9
jinshendan <jinshendan@gmail.com>
2020-06-26 10:08:18 +0200

    add cpp solution2

--- June-LeetCoding-Challenge/26-Sum-Root-to-Leaf-Numbers/Sum-Root-to-Leaf-Numbers.cpp
+++ June-LeetCoding-Challenge/26-Sum-Root-to-Leaf-Numbers/Sum-Root-to-Leaf-Numbers.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     int sumNumbers(TreeNode* root) {
         stack<TreeNode*> s;
@@ -24,5 +24,26 @@ public:
             }
         }
         return sum;
+    }
+};
+
+
+class Solution2 {
+public:
+    long long ans;
+    void dfs(TreeNode* root, long long x){
+        if (root == NULL) return;
+        if (root->left == NULL and root->right == NULL){
+            ans += x * 10 + root->val;
+            return ;
+        }
+        if (root->left) dfs(root->left, x * 10 + root->val);
+        if (root->right) dfs(root->right, x * 10 + root->val);
+    }
+    
+    int sumNumbers(TreeNode* root) {
+        ans = 0;
+        dfs(root, 0);
+        return ans;
     }
 };

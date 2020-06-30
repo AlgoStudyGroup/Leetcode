c7036021773bdbaf3ce2f09f8ede622728c049c3
FZ YUAN <42092999+FZYUAN-1@users.noreply.github.com>
2020-06-30 11:47:13 +0200

    Update Word-Search.cpp

--- June-LeetCoding-Challenge/30-Word-Search-II/Word-Search.cpp
+++ June-LeetCoding-Challenge/30-Word-Search-II/Word-Search.cpp
@@ -92,3 +92,95 @@ public:
                 
     }
 };
+
+
+
+
+const int dx[4] = { -1, 0, 1, 0 };
+const int dy[4] = { 0, -1, 0, 1 };
+#define ALPHABET_SIZE 26
+
+class Solution2 {
+private:
+	class Trie {
+        public:
+            bool isEnd;
+            bool isUsed;
+            unique_ptr<Trie> links[ALPHABET_SIZE];
+            /** Initialize your data structure here. */
+            Trie() {
+                this->isEnd = false;
+                this->isUsed = false;
+            }
+
+            bool containsChar(char ch) {
+                return links[ch - 'a'] != nullptr;
+            }
+            void put(char ch, Trie* newt) {
+                links[ch - 'a'].reset(newt);
+            }
+
+            Trie* get(char ch) {
+                return links[ch - 'a'].get();
+            }
+
+            /** Inserts a word into the trie. */
+            void insert(string word) {
+                // start from root node
+                Trie* root = this;
+                for (int i = 0; i < word.length(); i++) {
+                    // create a new node if path doesn't exists
+                    if (!root->containsChar(word[i])) {
+                        root->put(word[i], new Trie());
+                    }
+                    root = root->get(word[i]);
+                }
+                // make current node as a keyword
+                root->isEnd = true;
+            }
+
+            ~Trie() {}
+	};
+public:
+	vector<string> r;
+
+    vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
+		Trie* root = new Trie();
+		for (const string& s : words)
+			root->insert(s);
+
+		string acc;
+		for (int i = 0, lenx = board.size(); i < lenx; ++i) 
+			for (int j = 0, leny = board[0].size(); j < leny; ++j) 
+					explore(board, i, j, root, acc);
+        
+		delete root;
+		return r;
+
+    }
+
+	void explore(vector<vector<char>>& board, int i, int j, Trie* root, string& acc) {
+		char curr = board[i][j];
+		if (curr== '#' || !root->containsChar(curr)) return;
+		
+		acc.push_back(curr);
+		root = root->get(curr);
+		if (root->isEnd) {
+            if(!root->isUsed){      // in case of board = [["a","a"]], words = ["a"] 
+                r.push_back(acc);
+                root->isUsed = true;               
+            }
+		}
+        
+		board[i][j] = '*';
+		for (int k = 0; k < 4; ++k) {
+			int tox = i + dx[k], toy = j + dy[k];
+			if (tox < board.size() && tox >= 0 && toy >= 0 && toy < board[0].size() && board[tox][toy] != '*') 
+				explore(board, tox, toy, root, acc);
+			
+		}
+        
+		acc.pop_back();
+		board[i][j] = curr;
+	}
+};

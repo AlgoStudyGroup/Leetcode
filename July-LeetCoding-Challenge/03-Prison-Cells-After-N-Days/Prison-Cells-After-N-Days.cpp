0ee4179b547d1b4e352f3250f41a80198c50e80b
jinshendan <jinshendan@gmail.com>
2020-07-03 10:23:21 +0200

    add cpp solution

--- July-LeetCoding-Challenge/03-Prison-Cells-After-N-Days/Prison-Cells-After-N-Days.cpp
+++ July-LeetCoding-Challenge/03-Prison-Cells-After-N-Days/Prison-Cells-After-N-Days.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     vector<int> prisonAfterNDays(vector<int>& cells, int N) {
         // vector<vector<int>> memo;
@@ -17,6 +17,22 @@ public:
             memo.push_back(cpy);
             */
             swap(cells, cpy);
+        }
+        return cells;
+    }
+};
+
+
+class Solution2 {
+public:
+    vector<int> prisonAfterNDays(vector<int>& cells, int N) {      
+        N = N % 14;
+        if (N == 0) N = 14;
+        vector<int> v(8, 0); 
+        for (int j = 0; j < N; j++){
+            for(int i = 1; i < 7; i++) 
+                v[i] = (cells[i-1] == cells[i+1]) ? 1 : 0;
+            cells = v;
         }
         return cells;
     }

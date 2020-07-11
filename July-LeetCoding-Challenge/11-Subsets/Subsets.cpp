c821c3ab69152d113e302db63d9e1435284bdab7
jinshendan <jinshendan@gmail.com>
2020-07-11 11:20:25 +0200

    add cpp solution2

--- July-LeetCoding-Challenge/11-Subsets/Subsets.cpp
+++ July-LeetCoding-Challenge/11-Subsets/Subsets.cpp
@@ -16,3 +16,20 @@ public:
         }
     }
 };
+
+class Solution2 {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        ans.push_back(vector<int>());
+        for (int i = 0; i < nums.size(); i++){
+            int m = ans.size();
+            for (int j = 0; j < m; j++){
+                vector<int> v = ans[j];
+                v.push_back(nums[i]);
+                ans.push_back(v);
+            }
+        }
+        return ans;
+    }
+};

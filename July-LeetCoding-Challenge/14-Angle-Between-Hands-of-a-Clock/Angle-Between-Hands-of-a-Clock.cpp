b6125fde19fa97c35255424d588a596ff0182fd9
jinshendan <jinshendan@gmail.com>
2020-07-14 17:32:54 +0200

    add cpp solution

---
+++ July-LeetCoding-Challenge/14-Angle-Between-Hands-of-a-Clock/Angle-Between-Hands-of-a-Clock.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        double f1 = (hour % 12) * 30.0 + 0.5 * minutes;
+        double f2 = 6.0 * minutes;
+        double t = abs(f1 - f2);
+        return min(t, 360 - t);
+    }
+};

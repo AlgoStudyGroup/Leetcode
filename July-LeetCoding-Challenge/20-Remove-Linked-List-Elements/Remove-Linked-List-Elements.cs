ea696f611d55b2b9b0d24530f4a2788cf540bdd7
hellomrsun <soleilcharles@gmail.com>
2020-07-20 18:34:08 +0200

    Add Remove Linked List Elements C#

---
+++ July-LeetCoding-Challenge/20-Remove-Linked-List-Elements/Remove-Linked-List-Elements.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public ListNode RemoveElements(ListNode head, int val) {
+        if(head == null) return null;
+        var curr = new ListNode(-1);
+        curr.next = head;
+        head = curr;
+        while(curr.next != null){
+            if(curr.next.val == val){
+                curr.next = curr.next.next;
+            }else{
+                curr = curr.next;
+            }
+        }
+        return head.next;
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
